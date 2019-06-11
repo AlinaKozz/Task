0a2c5475664bdd7fc4160bad48d04b757308db12
AlinaKozz <alina.kozz58@gmail.com>
2019-06-11 11:02:03 +0300

    feat: add download file function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const inputArr = Array.prototype.slice.call(inputList);
 const saveButton = document.querySelector('.btn');
 var inputsValue = {};
 
+function fileDownload(url) {
+    let link = document.createElement('a');
+    link.setAttribute('download', 'file.json');
+    link.setAttribute('href', url);
+    document.body.appendChild(link);
+    link.click();
+}
+
 function createObject(obj) {
     inputArr.forEach(function (e) {
         let key = e.name,
@@ -23,6 +31,13 @@ function createObject(obj) {
     let buttons = '<button class="download">Download JSON</button>' +
         '<button class="print" onclick="window.print()">Print</button>';
     form.insertAdjacentHTML('beforeend', buttons);
+
+    const downloadButton = document.querySelector('.download');
+
+    downloadButton.addEventListener('click', function () {
+        let objectData = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+        fileDownload(objectData);
+    });
 
     return obj;
 }
